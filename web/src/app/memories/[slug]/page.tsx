7c8205aaaf391af93b85495428e28ee38a7db936
gdpnei2002 <joseneilson25@gmail.com>
2024-01-11 01:21:32 -0300

    melhorando identação

--- web/src/app/memories/[slug]/page.tsx
+++ web/src/app/memories/[slug]/page.tsx
@@ -28,32 +28,32 @@ export default async function MemoryPage(context: any) {
     },
   });
 
-  const memories = response.data;  
+  const memories = response.data;
 
   return (
     <div className="flex flex-col gap-10 p-8">
-            <Link
-              href="/"
-              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
-            >
-              Voltar
-              <ArrowLeft className="h-4 w-4" />
-            </Link>
-          <div className="space-y-4">
-            <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-              {dayjs(memories.createdAt).format('D[ de ]MMMM[, ]YYYY')}
-            </time>
-            <Image
-              src={memories.coverUrl}
-              alt=""
-              width={592}
-              height={280}
-              className="aspect-video w-full rounded-lg object-cover"
-            />
-            <p className="text-lg leading-relaxed text-gray-100">
-              {memories.content}
-            </p>
-          </div>
+      <Link
+        href="/"
+        className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+      >
+        <ArrowLeft className="h-4 w-4" />
+        Voltar
+      </Link>
+      <div className="space-y-4">
+        <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+          {dayjs(memories.createdAt).format("D[ de ]MMMM[, ]YYYY")}
+        </time>
+        <Image
+          src={memories.coverUrl}
+          alt=""
+          width={592}
+          height={280}
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+        <p className="text-lg leading-relaxed text-gray-100">
+          {memories.content}
+        </p>
+      </div>
     </div>
   );
 }
